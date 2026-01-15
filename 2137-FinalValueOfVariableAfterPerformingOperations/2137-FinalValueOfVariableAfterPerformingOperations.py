b5cbe1f60c3d6132d614edd079d555eb4e0c3221
Simon Cun <simoncun2586@gmail.com>
2026-01-14 17:17:49 -0800

    Create 2137-FinalValueOfVariableAfterPerformingOperations

---
+++ 2137-FinalValueOfVariableAfterPerformingOperations/2137-FinalValueOfVariableAfterPerformingOperations.py
@@ -0,0 +1,10 @@
+# Last updated: 1/14/2026, 5:17:46 PM
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        res = 0
+        for op in operations:
+            if '+' in op:
+                res += 1
+            else:
+                res -= 1
+        return res
