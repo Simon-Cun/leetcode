ae06cda124c8e4c8d4fc4b756e7e41fb425f1f4d
Simon Cun <simoncun2586@gmail.com>
2026-01-14 19:12:03 -0800

    File updated at 1/14/2026, 7:12:03 PM

--- 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
+++ 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
@@ -1,16 +1,17 @@
-# Last updated: 1/14/2026, 5:27:29 PM
+# Last updated: 1/14/2026, 7:12:03 PM
 1from collections import OrderedDict
 2class Solution:
 3    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
 4        res = l = 0
-5        
+5
 6        od = OrderedDict()
-7        for i, n in enumerate(nums):
-8            od[n] = i
-9            od.move_to_end(n)
-10            while len(od) > k:
-11                l = od.popitem(last=False)[1] + 1
-12            if len(od) == k:
-13                res += next(iter(od.items()))[1] - l + 1
-14
-15        return res
+7
+8        for i, n in enumerate(nums):
+9            od[n] = i
+10            od.move_to_end(n)
+11
+12            while len(od) > k:
+13                l = od.popitem(last=False)[1] + 1
+14            if len(od) == k:
+15                res += next(iter(od.items()))[1] - l + 1
+16        return res
