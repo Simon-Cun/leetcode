ec0baeefb83a10617452f78871bc6144ba5b28b3
Simon Cun <simoncun2586@gmail.com>
2026-01-14 17:18:18 -0800

    Create 154-FindMinimumInRotatedSortedArrayIi

---
+++ 154-FindMinimumInRotatedSortedArrayIi/154-FindMinimumInRotatedSortedArrayIi.py
@@ -0,0 +1,16 @@
+# Last updated: 1/14/2026, 5:18:15 PM
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+            if nums[r] < nums[mid]:
+                l = mid + 1
+            elif nums[r] > nums[mid]:
+                r = mid
+            else:
+                r -= 1
+
+        
+        return nums[l]
