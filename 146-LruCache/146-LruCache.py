506ba4a434e4687d20f37ed2dddc292c47793f05
Simon Cun <simoncun2586@gmail.com>
2026-01-14 17:18:20 -0800

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.py
@@ -0,0 +1,27 @@
+# Last updated: 1/14/2026, 5:18:17 PM
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.n = capacity
+        
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) == self.n and key not in self.cache:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
