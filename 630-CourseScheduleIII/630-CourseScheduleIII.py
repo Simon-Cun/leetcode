fc71f9ffcca183fc6e10dd9ca5f0c20103d6372c
Simon Cun <simoncun2586@gmail.com>
2026-01-18 11:20:11 -0800

    Create 630-CourseScheduleIII

---
+++ 630-CourseScheduleIII/630-CourseScheduleIII.py
@@ -0,0 +1,15 @@
+# Last updated: 1/18/2026, 11:20:09 AM
+1class Solution:
+2    def scheduleCourse(self, courses: List[List[int]]) -> int:
+3        courses.sort(key=lambda x : x[1])
+4        heap = []
+5        day = 0
+6
+7        for i, j in courses:
+8            heapq.heappush(heap, -i)
+9            day += i
+10
+11            if day > j:
+12                day += heapq.heappop(heap)
+13            
+14        return len(heap)
