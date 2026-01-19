5bd00f34ae66626fc5a442ce160145ba487c728b
Simon Cun <simoncun2586@gmail.com>
2026-01-18 20:06:32 -0800

    File updated at 1/18/2026, 8:06:32 PM

--- 135-Candy/135-Candy.py
+++ 135-Candy/135-Candy.py
@@ -1,6 +1,20 @@
-# Last updated: 1/18/2026, 8:05:30 PM
+# Last updated: 1/18/2026, 8:06:32 PM
 1class Solution:
-2    def divide(self, dividend: int, divisor: int) -> int:
-3        if dividend == -2**31 and divisor == -1:
-4            return 2**31 - 1
-5        return int(dividend / divisor)
+2    def candy(self, ratings: List[int]) -> int:
+3        n = len(ratings)
+4        candies = [1] * n
+5
+6        for i in range(n - 1):
+7            if ratings[i] > ratings[i + 1]:
+8                candies[i] = max(candies[i + 1] + 1, candies[i])
+9            elif ratings[i] < ratings[i + 1]:
+10                candies[i + 1] = max(candies[i] + 1, candies[i + 1])
+11        
+12        for i in range(n - 1, 0, -1):
+13            if ratings[i] > ratings[i - 1]:
+14                candies[i] = max(candies[i - 1] + 1, candies[i])
+15            elif ratings[i] < ratings[i - 1]:
+16                candies[i - 1] = max(candies[i] + 1, candies[i - 1])
+17        
+18        return sum(candies)
+19                
