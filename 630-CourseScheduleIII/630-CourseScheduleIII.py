f47e0a6f58e0ee29ef114b86c72ea146e9a426ef
Simon Cun <simoncun2586@gmail.com>
2026-01-19 10:42:56 -0800

    File updated at 1/19/2026, 10:42:54 AM

--- 630-CourseScheduleIII/630-CourseScheduleIII.py
+++ 630-CourseScheduleIII/630-CourseScheduleIII.py
@@ -1,15 +1,14 @@
-# Last updated: 1/19/2026, 10:42:30 AM
+# Last updated: 1/19/2026, 10:42:54 AM
 1class Solution:
 2    def scheduleCourse(self, courses: List[List[int]]) -> int:
 3        courses.sort(key=lambda x : x[1])
 4        heap = []
-5        day = 0
+5        time = 0
 6
 7        for duration, end in courses:
-8            heapq.heappush(heap, -duration)
-9            day += duration
-10
-11            if day > end:
-12                day += heapq.heappop(heap)
-13            
-14        return len(heap)
+8            time += duration
+9            heapq.heappush(heap, -duration)
+10            if time > end:
+11                time += heapq.heappop(heap)
+12        
+13        return len(heap)
