a95eeecc2efd9cb0e26c76a7c7fba43f10cebadd
Simon Cun <simoncun2586@gmail.com>
2026-01-18 20:22:31 -0800

    File updated at 1/18/2026, 8:22:31 PM

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.py
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.py
@@ -1,12 +1,46 @@
-# Last updated: 1/18/2026, 8:21:29 PM
+# Last updated: 1/18/2026, 8:22:31 PM
 1class Solution:
-2    def romanToInt(self, s: str) -> int:
-3        dictionary = { 'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
-4        summ = 0
-5        for i in range(1, len(s)):
-6            if dictionary[s[i - 1]] >= dictionary[s[i]]:
-7                summ += dictionary[s[i - 1]]
-8            else:
-9                summ -= dictionary[s[i - 1]]
-10        summ += dictionary[s[len(s) - 1]]
-11        return summ
+2    def intToRoman(self, num: int) -> str:
+3        roman = ""
+4        while num > 0:
+5            if num >= 1000:
+6                num -= 1000
+7                roman += "M"
+8            elif num >= 900:
+9                num -= 900
+10                roman += "CM"
+11            elif num >= 500:
+12                num -= 500
+13                roman += "D"
+14            elif num >= 400:
+15                num -= 400
+16                roman += "CD"
+17            elif num >= 100:
+18                num -= 100
+19                roman += "C"
+20            elif num >= 90:
+21                num -= 90
+22                roman += "XC"
+23            elif num >= 50:
+24                num -= 50
+25                roman += "L"
+26            elif num >= 40:
+27                num -= 40
+28                roman += "XL"
+29            elif num >= 10:
+30                num -= 10
+31                roman += "X"
+32            elif num >= 9:
+33                num -= 9
+34                roman += "IX"
+35            elif num >= 5:
+36                num -= 5
+37                roman += "V"
+38            elif num >= 4:
+39                num -= 4
+40                roman += "IV"
+41            else:
+42                num -= 1
+43                roman += "I"
+44        return roman
+45
