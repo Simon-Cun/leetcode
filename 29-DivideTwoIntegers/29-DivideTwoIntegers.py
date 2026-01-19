bf3a59823a69c7d52a2fca196898783ac5a72910
Simon Cun <simoncun2586@gmail.com>
2026-01-18 20:06:56 -0800

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.py
@@ -0,0 +1,6 @@
+# Last updated: 1/18/2026, 8:06:55 PM
+1class Solution:
+2    def divide(self, dividend: int, divisor: int) -> int:
+3        if dividend == -2**31 and divisor == -1:
+4            return 2**31 - 1
+5        return int(dividend / divisor)
