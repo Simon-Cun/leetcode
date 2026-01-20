bd39823e65bfc330bbac9306c5926f54a8271260
Simon Cun <simoncun2586@gmail.com>
2026-01-20 14:32:06 -0800

    File updated at 1/20/2026, 2:32:05 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,4 +1,4 @@
-# Last updated: 1/20/2026, 2:32:01 PM
+# Last updated: 1/20/2026, 2:32:05 PM
 1class Solution:
 2    def shuffle(self, nums: List[int], n: int) -> List[int]:
 3        res = []
@@ -7,8 +7,7 @@
 6            for i in nums[start::n]:
 7                res.append(i)
 8            start += 1
-9            
-10        
-11        return res
+9        
+10        return res
+11
 12
-13
