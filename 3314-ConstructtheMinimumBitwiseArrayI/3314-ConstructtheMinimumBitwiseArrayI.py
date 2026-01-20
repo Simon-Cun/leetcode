e06840d0140c4acfb20e47598669829d9c28a22d
Simon Cun <simoncun2586@gmail.com>
2026-01-20 13:20:18 -0800

    Create 3314-ConstructtheMinimumBitwiseArrayI

---
+++ 3314-ConstructtheMinimumBitwiseArrayI/3314-ConstructtheMinimumBitwiseArrayI.py
@@ -0,0 +1,11 @@
+# Last updated: 1/20/2026, 1:20:17 PM
+1class Solution:
+2    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+3        d = {}
+4        for i in range(1000, 0, -1):
+5            d[i | (i + 1)] = i
+6        res = []
+7        for i in nums:
+8            res.append(d.get(i, -1))
+9
+10        return res
