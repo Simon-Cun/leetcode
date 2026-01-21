8acebe8e29f39f5b5e1e48f22d1452e37dc565b2
Simon Cun <simoncun2586@gmail.com>
2026-01-21 10:07:40 -0800

    File updated at 1/21/2026, 10:07:39 AM

--- 461-HammingDistance/461-HammingDistance.py
+++ 461-HammingDistance/461-HammingDistance.py
@@ -1,4 +1,4 @@
-# Last updated: 1/21/2026, 10:04:22 AM
+# Last updated: 1/21/2026, 10:07:39 AM
 1class Solution:
 2    def hammingDistance(self, x: int, y: int) -> int:
-3        return sum(list(map(int, bin(x ^ y)[2:])))
+3        return (x ^ y).bit_count()
