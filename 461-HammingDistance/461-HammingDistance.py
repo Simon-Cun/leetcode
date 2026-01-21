9aab008900b252ded845fee6e1052d5667e2b585
Simon Cun <simoncun2586@gmail.com>
2026-01-21 10:04:22 -0800

    File updated at 1/21/2026, 10:04:22 AM

--- 461-HammingDistance/461-HammingDistance.py
+++ 461-HammingDistance/461-HammingDistance.py
@@ -1,11 +1,4 @@
-# Last updated: 1/21/2026, 10:02:56 AM
+# Last updated: 1/21/2026, 10:04:22 AM
 1class Solution:
 2    def hammingDistance(self, x: int, y: int) -> int:
-3        x ^= y
-4
-5        res = 0
-6        for i in bin(x)[2:]:
-7            if i == '1':
-8                res += 1
-9        
-10        return res
+3        return sum(list(map(int, bin(x ^ y)[2:])))
