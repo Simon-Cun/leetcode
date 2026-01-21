0aa13a9338bd386175d90c986a069bdc7eacca9f
Simon Cun <simoncun2586@gmail.com>
2026-01-21 10:07:46 -0800

    File updated at 1/21/2026, 10:07:45 AM

--- 461-HammingDistance/461-HammingDistance.py
+++ 461-HammingDistance/461-HammingDistance.py
@@ -1,4 +1,4 @@
-# Last updated: 1/21/2026, 10:07:39 AM
+# Last updated: 1/21/2026, 10:07:45 AM
 1class Solution:
 2    def hammingDistance(self, x: int, y: int) -> int:
 3        return (x ^ y).bit_count()
