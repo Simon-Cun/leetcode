90acdb24f9ececed98d35a5d354439633fb05d16
Simon Cun <simoncun2586@gmail.com>
2026-01-24 17:17:17 -0800

    File updated at 1/24/2026, 5:17:17 PM

--- 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.py
+++ 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.py
@@ -1,4 +1,4 @@
-# Last updated: 1/24/2026, 5:12:59 PM
+# Last updated: 1/24/2026, 5:17:17 PM
 1class Solution:
 2    def minimumDifference(self, nums: List[int], k: int) -> int:
 3        if k == 1:
@@ -9,6 +9,7 @@
 8        res = float('inf')
 9        for i in range(len(nums) - k + 1):
 10            res = min(res, nums[i] - nums[i + k - 1])
-11
-12        return res
-13
+11            print(res)
+12
+13        return res
+14# 9 7 4 1
