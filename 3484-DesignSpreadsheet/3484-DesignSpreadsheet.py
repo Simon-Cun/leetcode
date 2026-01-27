48a0fbc4c5feae264203e0371bb167ef66c831f9
Simon Cun <simoncun2586@gmail.com>
2026-01-26 17:22:12 -0800

    File updated at 1/26/2026, 5:22:12 PM

--- 3484-DesignSpreadsheet/3484-DesignSpreadsheet.py
+++ 3484-DesignSpreadsheet/3484-DesignSpreadsheet.py
@@ -1,4 +1,4 @@
-# Last updated: 1/26/2026, 5:21:52 PM
+# Last updated: 1/26/2026, 5:22:12 PM
 1class Spreadsheet:
 2
 3    def __init__(self, rows: int):
@@ -13,19 +13,18 @@
 12
 13    def getValue(self, formula: str) -> int:
 14        cell1, cell2 = formula[1:].split('+')
-15        print(cell1, cell2)
-16        if cell1.isnumeric() and cell2.isnumeric():
-17            return int(cell1) + int(cell2)
-18        elif cell1.isnumeric() and not cell2.isnumeric():
-19            return int(cell1) + self.spreadsheet.get(cell2, 0)
-20        elif not cell1.isnumeric() and cell2.isnumeric():
-21            return self.spreadsheet.get(cell1, 0) + int(cell2)
-22        else:
-23            return self.spreadsheet.get(cell1, 0) + self.spreadsheet.get(cell2, 0)
+15        if cell1.isnumeric() and cell2.isnumeric():
+16            return int(cell1) + int(cell2)
+17        elif cell1.isnumeric() and not cell2.isnumeric():
+18            return int(cell1) + self.spreadsheet.get(cell2, 0)
+19        elif not cell1.isnumeric() and cell2.isnumeric():
+20            return self.spreadsheet.get(cell1, 0) + int(cell2)
+21        else:
+22            return self.spreadsheet.get(cell1, 0) + self.spreadsheet.get(cell2, 0)
+23
 24
-25
-26# Your Spreadsheet object will be instantiated and called as such:
-27# obj = Spreadsheet(rows)
-28# obj.setCell(cell,value)
-29# obj.resetCell(cell)
-30# param_3 = obj.getValue(formula)
+25# Your Spreadsheet object will be instantiated and called as such:
+26# obj = Spreadsheet(rows)
+27# obj.setCell(cell,value)
+28# obj.resetCell(cell)
+29# param_3 = obj.getValue(formula)
