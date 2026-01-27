2d6be3f8aec83cace608381e2a28653ed57a8b8d
Simon Cun <simoncun2586@gmail.com>
2026-01-27 10:45:09 -0800

    File updated at 1/27/2026, 10:45:08 AM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,11 +1,11 @@
-# Last updated: 1/14/2026, 5:18:31 PM
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        one = 0
-        two = 1
-        num = 0
-        for i in range(n):
-            num = one + two
-            one = two
-            two = num
-        return num
+# Last updated: 1/27/2026, 10:45:08 AM
+1class Solution:
+2    def climbStairs(self, n: int) -> int:
+3        one = 0
+4        two = 1
+5        num = 0
+6        for i in range(n):
+7            num = one + two
+8            one = two
+9            two = num
+10        return num
