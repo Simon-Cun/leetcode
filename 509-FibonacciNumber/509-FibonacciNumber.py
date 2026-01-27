4e151a29ebe81b5fdfb4eb22ca9b77780aaf43b0
Simon Cun <simoncun2586@gmail.com>
2026-01-27 10:56:49 -0800

    File updated at 1/27/2026, 10:56:49 AM

--- 509-FibonacciNumber/509-FibonacciNumber.py
+++ 509-FibonacciNumber/509-FibonacciNumber.py
@@ -1,9 +1,10 @@
-# Last updated: 1/27/2026, 10:42:51 AM
+# Last updated: 1/27/2026, 10:56:49 AM
 1class Solution:
-2    def fib(self, n: int) -> int:
-3        if n == 0:
-4            return 0
-5        if n == 1:
-6            return 1
-7        
-8        return self.fib(n - 1) + self.fib(n - 2)
+2    @lru_cache(None)
+3    def fib(self, n: int) -> int:
+4        if n == 0:
+5            return 0
+6        if n == 1:
+7            return 1
+8        
+9        return self.fib(n - 1) + self.fib(n - 2)
