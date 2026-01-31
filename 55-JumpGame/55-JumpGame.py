b1533e64bf1a17d0f22edbb0dae8e6feb48bc30e
Simon Cun <simoncun2586@gmail.com>
2026-01-31 12:35:04 -0800

    File updated at 1/31/2026, 12:35:04 PM

--- 55-JumpGame/55-JumpGame.py
+++ 55-JumpGame/55-JumpGame.py
@@ -1,16 +1,11 @@
-# Last updated: 1/30/2026, 4:43:04 PM
+# Last updated: 1/31/2026, 12:35:04 PM
 1class Solution:
 2    def canJump(self, nums: List[int]) -> bool:
-3        curr = 0
-4        while nums[curr] < len(nums):
-5            if curr + nums[curr] >= len(nums) - 1:
-6                return True
-7            if nums[curr] == 0:
-8                return False
-9            temp = curr + 1
-10            for i in range(temp, curr + nums[curr] + 1):
-11                if i < len(nums) and nums[temp] <= nums[i] + i - temp and nums[i] != 0:
-12                    temp = i
-13            curr = temp
-14            
-15        return True
+3        gas = 0
+4        for n in nums:
+5            if gas < 0:
+6                return False
+7            elif gas < n:
+8                gas = n
+9            gas -= 1
+10        return True
