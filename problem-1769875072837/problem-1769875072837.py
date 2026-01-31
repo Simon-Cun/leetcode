9fa37adc55708e584a6538ce452b353c5b90ac55
Simon Cun <simoncun2586@gmail.com>
2026-01-31 07:57:53 -0800

    Create

---
+++ problem-1769875072837/problem-1769875072837.py
@@ -0,0 +1,11 @@
+# Last updated: 1/31/2026, 7:57:52 AM
+1class Solution:
+2    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+3        res = max(letters)
+4        for i in letters:
+5            if target < i < res:
+6                res = i
+7        if res > target:
+8            return res
+9        else:
+10            return letters[0]
