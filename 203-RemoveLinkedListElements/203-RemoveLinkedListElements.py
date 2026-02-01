1a1851fa0674a410685a207969f8e16f1fd03434
Simon Cun <simoncun2586@gmail.com>
2026-02-01 10:13:16 -0800

    File updated at 2/1/2026, 10:13:16 AM

--- 203-RemoveLinkedListElements/203-RemoveLinkedListElements.py
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.py
@@ -1,19 +1,16 @@
-# Last updated: 2/1/2026, 10:11:30 AM
+# Last updated: 2/1/2026, 10:13:16 AM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
 4#         self.val = val
 5#         self.next = next
 6class Solution:
-7    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-8        sentinel = ListNode(-1, head)
-9        prev = sentinel
-10        curr = head
-11        while curr:
-12            if curr.val == val:
-13                prev.next = curr.next
-14                curr = prev.next
-15            else:
-16                prev = curr
-17                curr = curr.next
-18        return sentinel.next
+7    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+8        slow = head
+9        fast = head
+10        
+11        while fast and fast.next:
+12            slow = slow.next
+13            fast = fast.next.next
+14        
+15        return slow
