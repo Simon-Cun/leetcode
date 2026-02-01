d5316601f80750beb9410cf2f73f4db77782575d
Simon Cun <simoncun2586@gmail.com>
2026-02-01 00:11:28 -0800

    File updated at 2/1/2026, 12:11:28 AM

--- 1721-SwappingNodesinaLinkedList/1721-SwappingNodesinaLinkedList.py
+++ 1721-SwappingNodesinaLinkedList/1721-SwappingNodesinaLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 2/1/2026, 12:10:38 AM
+# Last updated: 2/1/2026, 12:11:28 AM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, x):
@@ -11,10 +11,9 @@
 10        fast = head
 11
 12        while slow and fast and fast.next:
-13            fast = fast.next
-14            if slow == fast:
-15                return True
-16            slow = slow.next
-17            fast = fast.next
-18        
-19        return False
+13            slow = slow.next
+14            fast = fast.next.next
+15            if slow == fast:
+16                return True
+17        
+18        return False
