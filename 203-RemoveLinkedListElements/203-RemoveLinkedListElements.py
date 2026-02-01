72c48f987151c92e71f54e1d5e606d5c57c5c35f
Simon Cun <simoncun2586@gmail.com>
2026-02-01 10:16:38 -0800

    File updated at 2/1/2026, 10:16:38 AM

--- 203-RemoveLinkedListElements/203-RemoveLinkedListElements.py
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.py
@@ -1,13 +1,19 @@
-# Last updated: 2/1/2026, 10:16:03 AM
+# Last updated: 2/1/2026, 10:16:38 AM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
 4#         self.val = val
 5#         self.next = next
 6class Solution:
-7    def getDecimalValue(self, head: Optional[ListNode]) -> int:
-8        res = ""
-9        while head:
-10            res += str(head.val)
-11            head = head.next
-12        return int(res, 2)
+7    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+8        sentinel = ListNode(-1, head)
+9        prev = sentinel
+10        curr = head
+11        while curr:
+12            if curr.val == val:
+13                prev.next = curr.next
+14                curr = prev.next
+15            else:
+16                prev = curr
+17                curr = curr.next
+18        return sentinel.next
