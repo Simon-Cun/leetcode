a9c1f0f6d11d15f6f3b15b21049bc1c680a63d00
Simon Cun <simoncun2586@gmail.com>
2026-02-01 12:32:24 -0800

    File updated at 2/1/2026, 12:32:24 PM

--- 59-SpiralMatrixII/59-SpiralMatrixII.py
+++ 59-SpiralMatrixII/59-SpiralMatrixII.py
@@ -1,4 +1,4 @@
-# Last updated: 2/1/2026, 12:32:10 PM
+# Last updated: 2/1/2026, 12:32:24 PM
 1class Solution:
 2    def generateMatrix(self, n: int) -> List[List[int]]:
 3        spiral = [[0] * n for _ in range(n)]
@@ -17,13 +17,13 @@
 16                counter += 1
 17            r -= 1
 18
-19            if l <= r:
+19            if t <= b:
 20                for i in range(r, l - 1, -1):
 21                    spiral[b][i] = counter
 22                    counter += 1
 23                b -= 1
 24
-25            if  t <= b:
+25            if  l <= r:
 26                for i in range(b, t - 1, -1):
 27                    spiral[i][l] = counter
 28                    counter += 1
