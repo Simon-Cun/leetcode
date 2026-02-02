251858466f9c773912c7a8e348d4e52384048115
Simon Cun <simoncun2586@gmail.com>
2026-02-01 19:26:00 -0800

    File updated at 2/1/2026, 7:25:59 PM

--- 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
@@ -1,4 +1,4 @@
-# Last updated: 2/1/2026, 7:24:28 PM
+# Last updated: 2/1/2026, 7:25:59 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -6,18 +6,17 @@
 5#         self.next = next
 6class Solution:
 7    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8        sentinel = ListNode(-1, head)
-9        prev = sentinel
-10        curr = head
-11
-12        while curr:
-13            if curr and curr.next and curr.val == curr.next.val:
-14                while curr and curr.next and curr.val == curr.next.val:
-15                    curr = curr.next
-16                curr = curr.next
-17                prev.next = curr
-18                continue
-19            prev = curr
-20            curr = curr.next
-21
-22        return sentinel.next
+8        sentinel = prev = ListNode(-1, head)
+9        curr = head
+10
+11        while curr:
+12            if curr and curr.next and curr.val == curr.next.val:
+13                while curr and curr.next and curr.val == curr.next.val:
+14                    curr = curr.next
+15                curr = curr.next
+16                prev.next = curr
+17                continue
+18            prev = curr
+19            curr = curr.next
+20
+21        return sentinel.next
