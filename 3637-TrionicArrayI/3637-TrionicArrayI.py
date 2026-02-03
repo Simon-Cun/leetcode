739e6c0652147b07d29c5e32e4fb79d4aaf92560
Simon Cun <simoncun2586@gmail.com>
2026-02-03 14:41:08 -0800

    File updated at 2/3/2026, 2:41:07 PM

--- 3637-TrionicArrayI/3637-TrionicArrayI.py
+++ 3637-TrionicArrayI/3637-TrionicArrayI.py
@@ -1,19 +1,9 @@
-# Last updated: 2/2/2026, 5:07:18 PM
+# Last updated: 2/3/2026, 2:41:07 PM
 1class Solution:
-2    def isTrionic(self, nums: List[int]) -> bool:
-3        if len(nums) < 4:
-4            return False
-5        curr = 1
-6        i1 = False
-7        d = False
-8        i2 = False
-9        while curr < len(nums) and nums[curr - 1] < nums[curr]:
-10            curr += 1
-11            i1 = True
-12        while curr < len(nums) and nums[curr - 1] > nums[curr]:
-13            curr += 1
-14            d = True
-15        while curr < len(nums) and nums[curr - 1] < nums[curr]:
-16            curr += 1
-17            i2 = True
-18        return i1 and i2 and d and curr == len(nums)
+2    def sumZero(self, n: int) -> List[int]:
+3        res = set()
+4        for i in range(0 - n // 2, 0 + n // 2 + 1):
+5            res.add(i)
+6        if n % 2 == 0:
+7            res.remove(0)
+8        return list(res)
