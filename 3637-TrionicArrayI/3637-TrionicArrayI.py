049aadd3980be99833c0bf2e25c5444f2a3925d4
Simon Cun <simoncun2586@gmail.com>
2026-02-03 14:49:38 -0800

    File updated at 2/3/2026, 2:49:38 PM

--- 3637-TrionicArrayI/3637-TrionicArrayI.py
+++ 3637-TrionicArrayI/3637-TrionicArrayI.py
@@ -1,9 +1,7 @@
-# Last updated: 2/3/2026, 2:41:07 PM
+# Last updated: 2/3/2026, 2:49:38 PM
 1class Solution:
-2    def sumZero(self, n: int) -> List[int]:
-3        res = set()
-4        for i in range(0 - n // 2, 0 + n // 2 + 1):
-5            res.add(i)
-6        if n % 2 == 0:
-7            res.remove(0)
-8        return list(res)
+2    def getNoZeroIntegers(self, n: int) -> List[int]:
+3        for i in range(1, n):
+4            if '0' not in str(n - i) and '0' not in str(i):
+5                return [i, n - i]
+6        
