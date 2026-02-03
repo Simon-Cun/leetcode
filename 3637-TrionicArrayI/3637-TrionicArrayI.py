08cb439bf79f098ec72e47cff574cb9a2eda89de
Simon Cun <simoncun2586@gmail.com>
2026-02-03 14:58:34 -0800

    File updated at 2/3/2026, 2:58:34 PM

--- 3637-TrionicArrayI/3637-TrionicArrayI.py
+++ 3637-TrionicArrayI/3637-TrionicArrayI.py
@@ -1,7 +1,12 @@
-# Last updated: 2/3/2026, 2:49:38 PM
+# Last updated: 2/3/2026, 2:58:34 PM
 1class Solution:
-2    def getNoZeroIntegers(self, n: int) -> List[int]:
-3        for i in range(1, n):
-4            if '0' not in str(n - i) and '0' not in str(i):
-5                return [i, n - i]
-6        
+2    def maxFreqSum(self, s: str) -> int:
+3        d = {'a', 'e', 'i', 'o', 'u'}
+4        c_map = [0] * 26
+5        v_map = [0] * 26
+6        for i in s:
+7            if i in d:
+8                v_map[ord(i) - ord('a')] += 1
+9            else:
+10                c_map[ord(i) - ord('a')] += 1
+11        return max(v_map) + max(c_map)
