548d8741ffbaa7cc2fc174dae4e308a120434d91
Simon Cun <simoncun2586@gmail.com>
2026-02-03 18:12:07 -0800

    File updated at 2/3/2026, 6:12:07 PM

--- 135-Candy/135-Candy.py
+++ 135-Candy/135-Candy.py
@@ -1,4 +1,4 @@
-# Last updated: 1/18/2026, 8:06:32 PM
+# Last updated: 2/3/2026, 6:12:07 PM
 1class Solution:
 2    def candy(self, ratings: List[int]) -> int:
 3        n = len(ratings)
@@ -6,15 +6,14 @@
 5
 6        for i in range(n - 1):
 7            if ratings[i] > ratings[i + 1]:
-8                candies[i] = max(candies[i + 1] + 1, candies[i])
+8                candies[i] = max(candies[i], candies[i + 1] + 1)
 9            elif ratings[i] < ratings[i + 1]:
-10                candies[i + 1] = max(candies[i] + 1, candies[i + 1])
-11        
+10                candies[i + 1] = max(candies[i + 1], candies[i] + 1)
+11
 12        for i in range(n - 1, 0, -1):
 13            if ratings[i] > ratings[i - 1]:
-14                candies[i] = max(candies[i - 1] + 1, candies[i])
+14                candies[i] = max(candies[i], candies[i - 1] + 1)
 15            elif ratings[i] < ratings[i - 1]:
-16                candies[i - 1] = max(candies[i] + 1, candies[i - 1])
-17        
+16                candies[i - 1] = max(candies[i - 1], candies[i] + 1) 
+17
 18        return sum(candies)
-19                
