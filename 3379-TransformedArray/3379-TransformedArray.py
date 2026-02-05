ac1c8ecdce523f6ff42f3059777e51e70eaa0c17
Simon Cun <simoncun2586@gmail.com>
2026-02-04 21:19:41 -0800

    File updated at 2/4/2026, 9:19:41 PM

--- 3379-TransformedArray/3379-TransformedArray.py
+++ 3379-TransformedArray/3379-TransformedArray.py
@@ -1,13 +1,10 @@
-# Last updated: 2/4/2026, 9:18:39 PM
+# Last updated: 2/4/2026, 9:19:41 PM
 1class Solution:
 2    def constructTransformedArray(self, nums: List[int]) -> List[int]:
 3        n = len(nums)
 4        res = [0] * n
 5        for i in range(n):
-6            print(res)
-7            if nums[i] > 0:
-8                res[i] = nums[(i + nums[i]) % n]
-9            elif nums[i] < 0:
-10                res[i] = nums[(i - abs(nums[i])) % n]
-11        return res
-12
+6            if nums[i] != 0:
+7                res[i] = nums[(i + nums[i]) % n]
+8        return res
+9
