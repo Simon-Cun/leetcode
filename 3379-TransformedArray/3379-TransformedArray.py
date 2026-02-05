1271145b91e7f868401695c36074e356139530e0
Simon Cun <simoncun2586@gmail.com>
2026-02-05 14:55:57 -0800

    File updated at 2/5/2026, 2:55:57 PM

--- 3379-TransformedArray/3379-TransformedArray.py
+++ 3379-TransformedArray/3379-TransformedArray.py
@@ -1,10 +1,14 @@
-# Last updated: 2/4/2026, 9:19:48 PM
+# Last updated: 2/5/2026, 2:55:57 PM
 1class Solution:
-2    def constructTransformedArray(self, nums: List[int]) -> List[int]:
-3        n = len(nums)
-4        res = [0] * n
-5        for i in range(n):
-6            if nums[i] != 0:
-7                res[i] = nums[(i + nums[i]) % n]
-8        return res
-9
+2    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+3        arr.sort()
+4        res = []
+5        smallest_diff = float('inf')
+6        for i in range(len(arr) - 1):
+7            res.append([arr[i], arr[i + 1]])
+8        for i in range(len(res)) :
+9            smallest_diff = min(smallest_diff, res[i][1] - res[i][0])
+10        for i in range(len(res) - 1, -1, -1):
+11            if res[i][1] - res[i][0] != smallest_diff:
+12                res.pop(i)
+13        return res
