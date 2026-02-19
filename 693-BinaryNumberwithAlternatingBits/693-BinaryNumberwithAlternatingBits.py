7631ce85d186354e342637e06d10f5135bf57163
Simon Cun <simoncun2586@gmail.com>
2026-02-19 13:43:36 -0800

    File updated at 2/19/2026, 1:43:35 PM

--- 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.py
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.py
@@ -1,10 +1,8 @@
-# Last updated: 2/19/2026, 10:36:26 AM
+# Last updated: 2/19/2026, 1:43:35 PM
 1class Solution:
-2    def hasAlternatingBits(self, n: int) -> bool:
-3        s = bin(n)[2:]
-4        n = len(s)
-5        for i in range(1, n):
-6            if s[i] == s[i - 1]:
-7                return False
-8        return True
-9
+2    def reverseBits(self, n: int) -> int:
+3        s = bin(n)[2::]
+4        s = s.rjust(32, '0')
+5        s = s[::-1]
+6        print(s)
+7        return int(s, 2)
