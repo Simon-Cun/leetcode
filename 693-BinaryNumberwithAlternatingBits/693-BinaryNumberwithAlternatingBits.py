c4f684a31746ee9a50fe790987313d65b69d4ff0
Simon Cun <simoncun2586@gmail.com>
2026-02-19 10:36:26 -0800

    Create 693-BinaryNumberwithAlternatingBits

---
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.py
@@ -0,0 +1,10 @@
+# Last updated: 2/19/2026, 10:36:26 AM
+1class Solution:
+2    def hasAlternatingBits(self, n: int) -> bool:
+3        s = bin(n)[2:]
+4        n = len(s)
+5        for i in range(1, n):
+6            if s[i] == s[i - 1]:
+7                return False
+8        return True
+9
