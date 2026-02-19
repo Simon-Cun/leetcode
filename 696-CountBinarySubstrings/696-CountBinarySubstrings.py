c541ba27fad5764102bcbdd0986ecda663e8bf69
Simon Cun <simoncun2586@gmail.com>
2026-02-19 10:35:46 -0800

    File updated at 2/19/2026, 10:35:45 AM

--- 696-CountBinarySubstrings/696-CountBinarySubstrings.py
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.py
@@ -1,15 +1,10 @@
-# Last updated: 2/19/2026, 10:32:02 AM
+# Last updated: 2/19/2026, 10:35:45 AM
 1class Solution:
-2    def countBinarySubstrings(self, s: str) -> int:
-3        i, prev, count, n, res = 0, 0, 0, len(s), 0
-4        while i < n:
-5            if i == 0 or s[i] == s[i - 1]:
-6                count += 1
-7            else:
-8                res += min(prev, count)
-9                prev = count
-10                count = 1
-11            i += 1
-12        res += min(prev, count)
-13        return res
-14
+2    def hasAlternatingBits(self, n: int) -> bool:
+3        s = bin(n)[2:]
+4        n = len(s)
+5        for i in range(1, n):
+6            if s[i] == s[i - 1]:
+7                return False
+8        return True
+9
