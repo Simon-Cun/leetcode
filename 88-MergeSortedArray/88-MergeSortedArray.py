fb87de10244ea4c91548163436f6cb66465c4418
Simon Cun <simoncun2586@gmail.com>
2026-02-20 14:45:03 -0800

    File updated at 2/20/2026, 2:45:03 PM

--- 88-MergeSortedArray/88-MergeSortedArray.py
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -1,14 +1,13 @@
-# Last updated: 2/20/2026, 2:43:32 PM
+# Last updated: 2/20/2026, 2:45:03 PM
 1class Solution:
 2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 3        """
 4        Do not return anything, modify nums1 in-place instead.
 5        """
-6        nums2.reverse()
-7        i = 0
-8        while i < n + m:
-9            if nums2 and nums1[i] == 0:
-10                nums1[i] = nums2.pop()
-11            i += 1
-12        nums1.sort()
-13                
+6        i = 0
+7        while i < n + m:
+8            if nums2 and nums1[i] == 0:
+9                nums1[i] = nums2.pop()
+10            i += 1
+11        nums1.sort()
+12                
