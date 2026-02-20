261d6608647b7f26b3a29db18b48561175b01db3
Simon Cun <simoncun2586@gmail.com>
2026-02-20 15:26:57 -0800

    File updated at 2/20/2026, 3:26:56 PM

--- 88-MergeSortedArray/88-MergeSortedArray.py
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -1,4 +1,4 @@
-# Last updated: 2/20/2026, 3:26:48 PM
+# Last updated: 2/20/2026, 3:26:56 PM
 1class Solution:
 2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 3        """
@@ -8,17 +8,16 @@
 7        n -= 1
 8        r = len(nums1) - 1
 9        while nums2:
-10            print(m, n)
-11            if m == -1 or nums1[m] < nums2[n]:
-12                nums1[r] = nums2[n]
-13                r -= 1
-14                n -= 1
-15                nums2.pop()
-16            else:
-17                nums1[r] = nums1[m]
-18                nums1[m] = 0
-19                r -= 1
-20                m -= 1
-21        
+10            if m == -1 or nums1[m] < nums2[n]:
+11                nums1[r] = nums2[n]
+12                r -= 1
+13                n -= 1
+14                nums2.pop()
+15            else:
+16                nums1[r] = nums1[m]
+17                nums1[m] = 0
+18                r -= 1
+19                m -= 1
+20        
+21                
 22                
-23                
