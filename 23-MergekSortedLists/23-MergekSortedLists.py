8466e0568d5f11611f3d7f54c01c93ba92b85348
Simon Cun <simoncun2586@gmail.com>
2026-02-20 16:30:11 -0800

    Create 23-MergekSortedLists

---
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -0,0 +1,21 @@
+# Last updated: 2/20/2026, 4:30:11 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+8        res = []
+9        for i in lists:
+10            curr = i
+11            while curr:
+12                res.append(curr.val)
+13                curr = curr.next
+14        res.sort(reverse=True)
+15        head = None
+16        for i in res:
+17            to_push = ListNode(i)
+18            to_push.next = head
+19            head = to_push
+20        return head
