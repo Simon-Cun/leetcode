a271ab5a89729c7102b7324a7c2f7fb0943426be
Simon Cun <simoncun2586@gmail.com>
2026-02-20 17:19:12 -0800

    File updated at 2/20/2026, 5:19:12 PM

--- 762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.py
+++ 762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.py
@@ -1,15 +1,10 @@
-# Last updated: 2/20/2026, 5:16:18 PM
+# Last updated: 2/20/2026, 5:19:12 PM
 1class Solution:
 2    def countPrimeSetBits(self, left: int, right: int) -> int:
-3        def is_prime(n):
-4            if n < 2:
-5                return False
-6            for i in range(2, int(n**0.5) + 1):
-7                if n % i == 0:
-8                    return False
-9            return True
-10        res = 0
-11        for i in range(left, right + 1):
-12            if is_prime(i.bit_count()):
-13                res += 1
-14        return res
+3        primes = {2,3,5,7,11,13,17,19}
+4
+5        res = 0
+6        for i in range(left, right + 1):
+7            if i.bit_count() in primes:
+8                res += 1
+9        return res
