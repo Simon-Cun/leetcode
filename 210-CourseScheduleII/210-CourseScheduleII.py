6f39cfc5d1de84fb4d0a7be5e12a4d23a3a29a39
Simon Cun <simoncun2586@gmail.com>
2026-02-21 09:23:25 -0800

    File updated at 2/21/2026, 9:23:23 AM

--- 210-CourseScheduleII/210-CourseScheduleII.py
+++ 210-CourseScheduleII/210-CourseScheduleII.py
@@ -1,35 +1,29 @@
-# Last updated: 1/18/2026, 11:00:30 AM
+# Last updated: 2/21/2026, 9:23:23 AM
 1class Solution:
-2    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-3        g = [[] for _ in range(numCourses)]
-4        v = [-1] * numCourses
-5        res = []
-6
-7        for i, j in prerequisites:
-8            g[j].append(i)
-9
-10        def dfs(c):
-11            if v[c] == 0:
-12                return False
-13            
-14            if v[c] == 1:
-15                return True
-16            
-17            v[c] = 0
-18
-19            for i in g[c]:
-20                if not dfs(i):
-21                    return False
-22            
-23            v[c] = 1
-24            res.append(c)
-25
-26            return True
+2    def findOrder(self, n: int, prerequisites: List[List[int]]) -> List[int]:
+3        g = [[] for _ in range(n)]
+4        for i, j in prerequisites:
+5            g[j].append(i)
+6        
+7        v = [-1] * n
+8        stack = []
+9        def dfs(c):
+10            if v[c] == 0:
+11                return False
+12            if v[c] == 1:
+13                return True
+14            
+15            v[c] = 0
+16
+17            for i in g[c]:
+18                if not dfs(i):
+19                    return False
+20            stack.append(c)
+21            v[c] = 1
+22            return True
+23        for i in range(n):
+24            if v[i] == -1:
+25                if not dfs(i):
+26                    return []
 27        
-28        for i in range(numCourses):
-29            if v[i] == -1:
-30                if not dfs(i):
-31                    return []
-32        
-33        return res[::-1]
-34
+28        return stack[::-1]
