c280a99fec8a73f3199ba487f0ebda4a559f03ba
Simon Cun <simoncun2586@gmail.com>
2026-02-23 10:59:32 -0800

    File updated at 2/23/2026, 10:59:32 AM

--- 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.py
+++ 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.py
@@ -1,14 +1,15 @@
-# Last updated: 2/23/2026, 10:58:27 AM
+# Last updated: 2/23/2026, 10:59:32 AM
 1class Solution:
 2    def hasAllCodes(self, s: str, k: int) -> bool:
 3        n = len(s)
-4        codes = set()
-5        for i in range(k, n + 1):
-6            codes.add(s[i - k : i])
-7        print(codes)
-8        for i in range(1 << k):
-9            if bin(i)[2:].rjust(k, '0') not in codes:
-10                return False
-11        
-12        return True
-13
+4        
+5        codes = set()
+6        for i in range(k, n + 1):
+7            codes.add(s[i - k : i])
+8
+9        for i in range(1 << k):
+10            if bin(i)[2:].rjust(k, '0') not in codes:
+11                return False
+12        
+13        return True
+14
