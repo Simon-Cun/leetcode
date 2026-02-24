55a4afae28ad957814bec812014b6aec3088f2d9
Simon Cun <simoncun2586@gmail.com>
2026-02-23 16:06:21 -0800

    File updated at 2/23/2026, 4:06:21 PM

--- 1323-Maximum69Number/1323-Maximum69Number.py
+++ 1323-Maximum69Number/1323-Maximum69Number.py
@@ -1,12 +1,14 @@
-# Last updated: 2/23/2026, 4:05:40 PM
+# Last updated: 2/23/2026, 4:06:21 PM
 1class Solution:
-2    def maximum69Number (self, num: int) -> int:
-3        l = []
-4        flag = True
-5        for i in str(num):
-6            if int(i) < 9 and flag:
-7                flag = False
-8                l.append(9)
-9            else:
-10                l.append(i)
-11        return int(''.join(map(str, l)))
+2    def maximum69Number(self, num: int) -> int:
+3        res = []
+4        changed = False
+5
+6        for d in str(num):
+7            if d == '6' and not changed:
+8                res.append('9')
+9                changed = True
+10            else:
+11                res.append(d)
+12
+13        return int(''.join(res))
