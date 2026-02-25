940f8c47dc65f0c5a36a020966b5b2944d2f7b89
Simon Cun <simoncun2586@gmail.com>
2026-02-24 16:21:53 -0800

    Create 1356-SortIntegersbyTheNumberof1Bits

---
+++ 1356-SortIntegersbyTheNumberof1Bits/1356-SortIntegersbyTheNumberof1Bits.py
@@ -0,0 +1,10 @@
+# Last updated: 2/24/2026, 4:21:52 PM
+1class Solution:
+2    def sortByBits(self, arr: List[int]) -> List[int]:
+3        pairs = []
+4        for i in arr:
+5            pairs.append([i, i.bit_count()])
+6        pairs.sort(key=lambda x : x[0])
+7        pairs.sort(key=lambda x : x[1])
+8
+9        return [row[0] for row in pairs]
