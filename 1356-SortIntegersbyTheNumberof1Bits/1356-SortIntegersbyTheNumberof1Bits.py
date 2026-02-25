165c605ddd05317a642b2e1f1e9438e8c8724088
Simon Cun <simoncun2586@gmail.com>
2026-02-24 17:01:59 -0800

    File updated at 2/24/2026, 5:01:59 PM

--- 1356-SortIntegersbyTheNumberof1Bits/1356-SortIntegersbyTheNumberof1Bits.py
+++ 1356-SortIntegersbyTheNumberof1Bits/1356-SortIntegersbyTheNumberof1Bits.py
@@ -1,10 +1,24 @@
-# Last updated: 2/24/2026, 4:22:15 PM
-1class Solution:
-2    def sortByBits(self, arr: List[int]) -> List[int]:
-3        pairs = []
-4        for i in arr:
-5            pairs.append([i, i.bit_count()])
-6        pairs.sort(key=lambda x : x[0])
-7        pairs.sort(key=lambda x : x[1])
-8
-9        return [row[0] for row in pairs]
+# Last updated: 2/24/2026, 5:01:59 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+9        arr = []
+10        def dfs(curr, s):
+11            if not curr:
+12                return
+13            if not curr.left and not curr.right:
+14                arr.append(s + str(curr.val))
+15                return
+16            dfs(curr.left, s + str(curr.val))
+17            dfs(curr.right, s + str(curr.val))
+18        dfs(root, "")
+19        res = 0
+20        for i in arr:
+21            res += int(i, 2)
+22        return res
+23
