b65ef3d77048ec5ca61d178aadaa317b07f2c4cc
Simon Cun <simoncun2586@gmail.com>
2026-02-26 09:59:09 -0800

    File updated at 2/26/2026, 9:59:09 AM

--- 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
+++ 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
@@ -1,12 +1,14 @@
-# Last updated: 2/26/2026, 9:50:12 AM
+# Last updated: 2/26/2026, 9:59:09 AM
 1class Solution:
-2    def numSteps(self, l: str) -> int:
+2    def numSteps(self, s: str) -> int:
 3        res = 0
-4        s = int(l, 2)
-5        while s >= 2:
-6            if s % 2 == 0:
-7                s //= 2
+4        num = int(s, 2)
+5        while num > 1:
+6            if num & 1:
+7                num += 1
 8            else:
-9                s += 1
+9                num >>= 1
 10            res += 1
 11        return res
+12        
+13
