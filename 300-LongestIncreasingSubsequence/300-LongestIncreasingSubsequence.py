02177f28300433324a38ce8f67f60bbdb2177ed5
Simon Cun <simoncun2586@gmail.com>
2026-03-03 18:48:14 -0800

    File updated at 3/3/2026, 6:48:14 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.py
@@ -1,11 +1,10 @@
-# Last updated: 3/3/2026, 6:43:27 PM
-1from functools import lru_cache
-2class Solution:
-3    def lengthOfLIS(self, nums: List[int]) -> int:
-4        n = len(nums)
-5        dp = [1] * n
-6        for i in range(n):
-7            for j in range(i):
-8                if nums[j] < nums[i]:
-9                    dp[i] = max(dp[i], dp[j] + 1)
-10        return max(dp)
+# Last updated: 3/3/2026, 6:48:14 PM
+1class Solution:
+2    def lengthOfLIS(self, nums: List[int]) -> int:
+3        n = len(nums)
+4        dp = [1] * n
+5        for i in range(n):
+6            for j in range(i):
+7                if nums[j] < nums[i]:
+8                    dp[i] = max(dp[i], dp[j] + 1)
+9        return max(dp)
