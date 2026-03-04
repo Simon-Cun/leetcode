2c2824e39d442c143d9d7ecc9dd8eca6a5988650
Simon Cun <simoncun2586@gmail.com>
2026-03-04 14:58:08 -0800

    File updated at 3/4/2026, 2:58:08 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,4 +1,4 @@
-# Last updated: 3/4/2026, 1:41:46 PM
+# Last updated: 3/4/2026, 2:58:08 PM
 1class Solution:
 2    def longestPalindrome(self, s: str) -> str:
 3        n = len(s)
@@ -10,16 +10,15 @@
 9            if s[i]== s[i + 1]:
 10                dp[i][i + 1] = True
 11
-12        
-13        for i in range(n - 3, -1, -1):
-14            for j in range(n - 1, i + 1, -1):
-15                if s[i] == s[j] and dp[i + 1][j - 1]:
-16                    dp[i][j] = True
-17        res = s[0]
-18        for i in range(n):
-19            for j in range(n):
-20                if dp[i][j] == True:
-21                    if len(res) < len(s[i:j + 1]):
-22                        res = s[i:j + 1]
-23        return res
-24        
+12        for i in range(n - 3, -1, -1):
+13            for j in range(n - 1, i + 1, -1):
+14                if s[i] == s[j] and dp[i + 1][j - 1]:
+15                    dp[i][j] = True
+16        res = s[0]
+17        for i in range(n):
+18            for j in range(n):
+19                if dp[i][j] == True:
+20                    if len(res) < len(s[i:j + 1]):
+21                        res = s[i:j + 1]
+22        return res
+23        
