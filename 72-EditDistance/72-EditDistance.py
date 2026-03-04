30f92806f47500943724659698eaebe428ca0410
Simon Cun <simoncun2586@gmail.com>
2026-03-04 10:46:28 -0800

    File updated at 3/4/2026, 10:46:27 AM

--- 72-EditDistance/72-EditDistance.py
+++ 72-EditDistance/72-EditDistance.py
@@ -1,18 +1,23 @@
-# Last updated: 3/4/2026, 10:20:52 AM
-1from functools import lru_cache
-2class Solution:
-3    @lru_cache(None)
-4    def minDistance(self, word1: str, word2: str) -> int:
-5        if not word1:
-6            return len(word2)
-7        if not word2:
-8            return len(word1)
-9        
-10        if word1[0] == word2[0]:
-11            return self.minDistance(word1[1:], word2[1:])
-12        
-13        insert = self.minDistance(word1, word2[1:])
-14        delete = self.minDistance(word1[1:], word2)
-15        replace = self.minDistance(word1[1:], word2[1:])
-16
-17        return 1 + min(insert, delete, replace)
+# Last updated: 3/4/2026, 10:46:27 AM
+1class Solution:
+2    def minDistance(self, word1: str, word2: str) -> int:
+3        n, m = len(word1), len(word2)
+4
+5        dp  =[[0] * (m + 1) for _ in range(n + 1)]
+6        
+7        for i in range(m + 1):
+8            dp[0][i] = i
+9        for i in range(n + 1):
+10            dp[i][0] = i
+11        
+12        for i in range(1, n + 1):
+13            for j in range(1, m + 1):
+14                if word1[i - 1] == word2[j - 1]:
+15                    dp[i][j] = dp[i - 1][j - 1]
+16                else:
+17                    insert = dp[i][j - 1] + 1
+18                    delete = dp[i - 1][j] + 1
+19                    replace = dp[i - 1][j - 1] + 1
+20
+21                    dp[i][j] = min(insert, delete, replace)
+22        return dp[n][m]
