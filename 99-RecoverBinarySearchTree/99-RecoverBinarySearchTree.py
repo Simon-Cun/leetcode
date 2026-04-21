8b06893ae6b3ebb0ef2119c6a730129de366cc4e
Simon Cun <simoncun2586@gmail.com>
2026-04-21 10:49:44 -0700

    File updated at 4/21/2026, 10:49:44 AM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.py
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 4/21/2026, 10:30:31 AM
+# Last updated: 4/21/2026, 10:49:44 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -6,22 +6,14 @@
 5#         self.left = left
 6#         self.right = right
 7class Solution:
-8    def recoverTree(self, root: Optional[TreeNode]) -> None:
-9        """
-10        Do not return anything, modify root in-place instead.
-11        """
-12        arr = []
-13        def dfs(curr):
-14            if not curr:
-15                return
-16            dfs(curr.left)
-17            arr.append(curr)
-18            dfs(curr.right)
-19        dfs(root)
-20        first, second = None, None
-21        for i in range(1, len(arr)):
-22            if arr[i - 1].val > arr[i].val:
-23                if first is None:
-24                    first = arr[i - 1]
-25                second = arr[i]
-26        first.val, second.val = second.val, first.val
+8    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+9        def dfs(curr1, curr2):
+10            if not curr1 and not curr2:
+11                return True
+12            if not curr1 or not curr2:
+13                return False
+14            if curr1.val != curr2.val:
+15                return False
+16            return dfs(curr1.left, curr2.left) and dfs(curr1.right, curr2.right)
+17        return dfs(p, q)
+18            
