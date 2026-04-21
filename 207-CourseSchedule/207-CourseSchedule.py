615f61927b4b678554fdecab7b5598137cc6742b
Simon Cun <simoncun2586@gmail.com>
2026-04-21 09:54:31 -0700

    File updated at 4/21/2026, 9:54:31 AM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,33 +1,27 @@
-# Last updated: 1/14/2026, 5:18:11 PM
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        v = [-1] * numCourses
-
-        g = [[] for _ in range(numCourses)]
-
-        for i, j in prerequisites:
-            g[j].append(i)
-
-        def dfs(c):
-            if v[c] == 0:
-                return False
-            
-            if v[c] == 1:
-                return True
-
-            v[c] = 0
-
-            for i in g[c]:
-                if not dfs(i):
-                    return False
-            
-            v[c] = 1
-            
-            return True
-    
-        for i in range(numCourses):
-            if v[i] == -1:
-                if not dfs(i):
-                    return False
-
-        return True
+# Last updated: 4/21/2026, 9:54:31 AM
+1class Solution:
+2    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
+3        g = [[] for _ in range(n)]
+4        for i, j in prerequisites:
+5            g[j].append(i)
+6            
+7        v = [-1] * n
+8        def dfs(c):
+9            if v[c] == 0:
+10                return False
+11            
+12            if v[c] == 1:
+13                return True
+14
+15            v[c] = 0
+16            for i in g[c]:
+17                if not dfs(i):
+18                    return False
+19            
+20            v[c] = 1
+21            return True
+22        for i in range(n):
+23            if v[i] == -1:
+24                if not dfs(i):
+25                    return False
+26        return True
