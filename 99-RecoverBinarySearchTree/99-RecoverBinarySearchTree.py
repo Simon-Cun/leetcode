b7745dae74f64ba9155a3ee9cff49c79ed9e9876
Simon Cun <simoncun2586@gmail.com>
2026-04-21 10:53:38 -0700

    File updated at 4/21/2026, 10:53:38 AM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.py
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 4/21/2026, 10:49:44 AM
+# Last updated: 4/21/2026, 10:53:38 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -6,14 +6,13 @@
 5#         self.left = left
 6#         self.right = right
 7class Solution:
-8    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-9        def dfs(curr1, curr2):
-10            if not curr1 and not curr2:
+8    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+9        def dfs(l, r):
+10            if not r and not l:
 11                return True
-12            if not curr1 or not curr2:
+12            if not r or not l:
 13                return False
-14            if curr1.val != curr2.val:
+14            if r.val != l.val:
 15                return False
-16            return dfs(curr1.left, curr2.left) and dfs(curr1.right, curr2.right)
-17        return dfs(p, q)
-18            
+16            return dfs(l.left, r.right) and dfs(l.right, r.left)
+17        return dfs(root.left, root.right)
