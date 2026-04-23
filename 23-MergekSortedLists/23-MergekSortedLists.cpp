cf15d2ef635785c6f3dfbe42040528f938684bb9
Simon Cun <simoncun2586@gmail.com>
2026-04-23 15:23:45 -0700

    File updated at 4/23/2026, 3:23:45 PM

--- 23-MergekSortedLists/23-MergekSortedLists.cpp
+++ 23-MergekSortedLists/23-MergekSortedLists.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/23/2026, 3:18:07 PM
+// Last updated: 4/23/2026, 3:23:45 PM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -14,23 +14,21 @@
 13    ListNode* mergeKLists(vector<ListNode*>& lists) {
 14        auto comp = [](const ListNode* a, const ListNode* b) { return a->val > b->val; };
 15        priority_queue<ListNode*, vector<ListNode*>, decltype(comp)> pq(comp);
-16        for (auto& i : lists) {
-17            while (i) {
-18                pq.push(i);
-19                i = i->next;
-20            }
-21        }
-22        if (pq.empty()) return nullptr;
-23        
-24        ListNode* head = pq.top();
-25        pq.pop();
-26        ListNode* tail = head;
-27        while (!pq.empty()) {
-28            tail->next = pq.top();
-29            pq.pop();
-30            tail = tail->next;
-31        }
-32        tail->next = nullptr;
-33        return head;
-34    }
-35};
+16        for (auto& i : lists) if (i) pq.push(i);
+17
+18        if (pq.empty()) return nullptr;
+19        
+20        ListNode* head = pq.top();
+21        pq.pop();
+22        if (head->next) pq.push(head->next);
+23        ListNode* tail = head;
+24        while (!pq.empty()) {
+25            tail->next = pq.top();
+26            pq.pop();
+27            tail = tail->next;
+28            if (tail->next) pq.push(tail->next);
+29        }
+30        tail->next = nullptr;
+31        return head;
+32    }
+33};
