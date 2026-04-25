3521a718dd8df8829ec2365fb4cc2ca50d4e5cf5
Simon Cun <simoncun2586@gmail.com>
2026-04-25 09:01:26 -0700

    File updated at 4/25/2026, 9:01:23 AM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -1,13 +1,16 @@
-// Last updated: 4/24/2026, 5:02:32 PM
+// Last updated: 4/25/2026, 9:01:23 AM
 1class Solution {
 2public:
-3    int removeDuplicates(vector<int>& nums) {
-4        int k = 1;
-5        for (int i = 1; i < nums.size(); ++i) {
-6            if (nums.at(i) != nums.at(i - 1)) {
-7                nums.at(k++) = nums.at(i);
-8            }
-9        }
-10        return k;
-11    }
-12};
+3    string longestCommonPrefix(vector<string>& strs) {
+4        sort(strs.begin(), strs.end());
+5        string prefix = "";
+6        for (int i = 0; i < min(strs.at(0).size(), strs.at(strs.size() - 1).size()); ++i) {
+7            if (strs.at(0).at(i) == strs.at(strs.size() - 1).at(i)) {
+8                prefix += strs.at(0).at(i);
+9            } else {
+10                break;
+11            }
+12        }
+13        return prefix;
+14    }
+15};
