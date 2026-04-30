6503b2079e029cb1401c56711cd0603787e36b2b
Simon Cun <simoncun2586@gmail.com>
2026-04-29 18:18:28 -0700

    File updated at 4/29/2026, 6:18:28 PM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,9 +1,9 @@
-# Last updated: 4/29/2026, 6:16:27 PM
+# Last updated: 4/29/2026, 6:18:28 PM
 1class Solution:
 2    def checkInclusion(self, s1: str, s2: str) -> bool:
 3        c1 = [0] * 26
-4        for c in s1:
-5            c1[ord(c) - ord('a')] += 1
+4        for c in s1: c1[ord(c) - ord('a')] += 1
+5        
 6        l = 0
 7        c2 = [0] * 26
 8        for r in range(len(s2)):
