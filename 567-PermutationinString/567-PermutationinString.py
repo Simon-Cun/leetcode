11f3dc8f48f1f6f8bc6a762e78d715bf7a126fbe
Simon Cun <simoncun2586@gmail.com>
2026-04-29 18:16:27 -0700

    File updated at 4/29/2026, 6:16:27 PM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,19 +1,15 @@
-# Last updated: 4/29/2026, 6:16:00 PM
+# Last updated: 4/29/2026, 6:16:27 PM
 1class Solution:
 2    def checkInclusion(self, s1: str, s2: str) -> bool:
 3        c1 = [0] * 26
-4        for i in s1:
-5            c1[ord(i) - ord('a')] += 1
+4        for c in s1:
+5            c1[ord(c) - ord('a')] += 1
 6        l = 0
 7        c2 = [0] * 26
-8        for i in range(len(s2)):
-9            print(l, i)
-10            print(c1)
-11            print(c2)
-12            print()
-13            c2[ord(s2[i]) - ord('a')] += 1
-14            if i >= len(s1):
-15                c2[ord(s2[l]) - ord('a')] -= 1
-16                l += 1
-17            if c1 == c2: return True
-18        return False
+8        for r in range(len(s2)):
+9            c2[ord(s2[r]) - ord('a')] += 1
+10            if r >= len(s1):
+11                c2[ord(s2[l]) - ord('a')] -= 1
+12                l += 1
+13            if c1 == c2: return True
+14        return False
