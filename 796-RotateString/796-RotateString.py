30092fea601560904882f132cb57f41ccff1a0f2
Simon Cun <simoncun2586@gmail.com>
2026-05-03 12:59:42 -0700

    File updated at 5/3/2026, 12:59:42 PM

--- 796-RotateString/796-RotateString.py
+++ 796-RotateString/796-RotateString.py
@@ -1,9 +1,4 @@
-# Last updated: 5/3/2026, 12:58:15 PM
+# Last updated: 5/3/2026, 12:59:42 PM
 1class Solution:
 2    def rotateString(self, s: str, goal: str) -> bool:
-3        count = 0
-4        while count < len(s):
-5            rotated = s[len(s) - count:] + s[:len(s) - count]
-6            if rotated == goal: return True
-7            count += 1
-8        return False
+3        return len(s) == len(goal) and goal in s + s
