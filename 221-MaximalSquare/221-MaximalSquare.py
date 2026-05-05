963af45339b2db628b293f3dda1e43c12b4f86db
Simon Cun <simoncun2586@gmail.com>
2026-05-04 23:24:39 -0700

    Create 221-MaximalSquare

---
+++ 221-MaximalSquare/221-MaximalSquare.py
@@ -0,0 +1,19 @@
+# Last updated: 5/4/2026, 11:24:38 PM
+1class Solution:
+2    def maximalSquare(self, matrix: List[List[str]]) -> int:
+3        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+4
+5        for i in range(len(matrix)): dp[i][0] = int(matrix[i][0]) # base case
+6        for i in range(len(matrix[0])): dp[0][i] = int(matrix[0][i]) # base case
+7
+8        for i in range(1, len(matrix)):
+9            for j in range(1, len(matrix[0])):
+10                if matrix[i][j] == '1':
+11                    dp[i][j] = min(int(dp[i - 1][j]), int(dp[i][j - 1]), int(dp[i - 1][j - 1])) + 1 # recurrence
+12        print(dp)
+13        # getting the result
+14        ret = 0
+15        for i in dp:
+16            ret = max(ret, max(i))
+17        return ret * ret
+18        
