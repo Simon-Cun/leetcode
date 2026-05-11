bfb36dd780e31181229953cd8c62ec29c1d2c8a9
Simon Cun <simoncun2586@gmail.com>
2026-05-10 19:19:59 -0700

    File updated at 5/10/2026, 7:19:59 PM

--- 2553-SeparatetheDigitsinanArray/2553-SeparatetheDigitsinanArray.cpp
+++ 2553-SeparatetheDigitsinanArray/2553-SeparatetheDigitsinanArray.cpp
@@ -1,10 +1,9 @@
-// Last updated: 5/10/2026, 7:17:13 PM
+// Last updated: 5/10/2026, 7:19:59 PM
 1class Solution {
 2public:
 3    vector<int> separateDigits(vector<int>& nums) {
 4        vector<int> ret;
-5        for (auto& i : nums) for (auto& j : to_string(i)) ret.push_back(j - 
-6        '0');
-7        return ret;
-8    }
-9};
+5        for (auto& i : nums) for (auto& j : to_string(i)) ret.push_back(j - '0');
+6        return ret;
+7    }
+8};
