b92063f6af7e4f4f976b7b8f93cff89d7cbb21f2
Simon Cun <simoncun2586@gmail.com>
2026-05-11 18:53:40 -0700

    File updated at 5/11/2026, 6:53:40 PM

--- 1665-MinimumInitialEnergytoFinishTasks/1665-MinimumInitialEnergytoFinishTasks.cpp
+++ 1665-MinimumInitialEnergytoFinishTasks/1665-MinimumInitialEnergytoFinishTasks.cpp
@@ -1,20 +1,19 @@
-// Last updated: 5/11/2026, 6:47:01 PM
+// Last updated: 5/11/2026, 6:53:40 PM
 1class Solution {
 2public:
 3    int minimumEffort(vector<vector<int>>& tasks) {
 4        vector<tuple<int, int, int>> arr;
-5        for (auto& i : tasks) {
-6            arr.push_back({i[1] - i[0], i[0], i[1]});
-7        }
-8        sort(arr.begin(), arr.end(), greater<>());
-9        int ret = 0, curr = 0;
-10        for (auto& [a, b, c] : arr) {
-11            if (curr < c) {
-12                ret += c - curr;
-13                curr = c;
-14            }
-15            curr -= b;
-16        }
-17        return ret;
-18    }
-19};
+5        for (auto& i : tasks) arr.push_back({i[1] - i[0], i[0], i[1]});
+6        sort(arr.begin(), arr.end(), greater<>());
+7
+8        int ret = 0, curr = 0;
+9        for (auto& [a, b, c] : arr) {
+10            if (curr < c) {
+11                ret += c - curr;
+12                curr = c;
+13            }
+14            curr -= b;
+15        }
+16        return ret;
+17    }
+18};
