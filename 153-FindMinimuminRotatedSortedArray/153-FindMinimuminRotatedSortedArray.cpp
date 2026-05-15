3034a40978a02e5e2e1223928cc187892dad6875
Simon Cun <simoncun2586@gmail.com>
2026-05-15 10:45:48 -0700

    File updated at 5/15/2026, 10:45:48 AM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.cpp
@@ -1,17 +1,16 @@
-// Last updated: 5/15/2026, 10:45:24 AM
+// Last updated: 5/15/2026, 10:45:48 AM
 1class Solution {
 2public:
 3    int findMin(vector<int>& nums) {
 4        int l = 0, r = nums.size() - 1;
 5        while (l < r) {
 6            int m = l + (r - l) / 2;
-7            cout << l << ' ' << m << ' ' << r << endl;
-8            if (nums.at(m) > nums.at(r)) {
-9                l = m + 1;
-10            } else {
-11                r = m;
-12            }
-13        }
-14        return nums.at(l);
-15    }
-16};
+7            if (nums.at(m) > nums.at(r)) {
+8                l = m + 1;
+9            } else {
+10                r = m;
+11            }
+12        }
+13        return nums.at(l);
+14    }
+15};
