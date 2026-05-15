92b48eaf660cf7cdc9efff691763125e3d2d047f
Simon Cun <simoncun2586@gmail.com>
2026-05-15 10:58:33 -0700

    File updated at 5/15/2026, 10:58:32 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,28 +1,25 @@
-// Last updated: 5/15/2026, 10:57:59 AM
+// Last updated: 5/15/2026, 10:58:32 AM
 1class Solution {
 2public:
 3    int search(vector<int>& nums, int target) {
 4        int l = 0, r = nums.size() - 1;
 5        while (l <= r) {
 6            int m = l + (r - l) / 2;
-7            if (nums.at(m) > nums.at(r)) {
-8                if (nums.at(m) == target) {
-9                    return m;
-10                } else if (nums.at(l) <= target and target < nums.at(m)) {
-11                    r = m - 1;
-12                } else {
-13                    l = m + 1;
-14                }
-15            } else {
-16                if (nums.at(m) == target) {
-17                    return m;
-18                } else if (nums.at(m) < target and target <= nums.at(r)) {
-19                    l = m + 1;
-20                } else {
-21                    r = m - 1;
-22                }
-23            }
-24        }
-25        return -1;
-26    }
-27};
+7            if (nums.at(m) == target) return m;
+8            if (nums.at(m) > nums.at(r)) {
+9                if (nums.at(l) <= target and target < nums.at(m)) {
+10                    r = m - 1;
+11                } else {
+12                    l = m + 1;
+13                }
+14            } else {
+15                if (nums.at(m) < target and target <= nums.at(r)) {
+16                    l = m + 1;
+17                } else {
+18                    r = m - 1;
+19                }
+20            }
+21        }
+22        return -1;
+23    }
+24};
