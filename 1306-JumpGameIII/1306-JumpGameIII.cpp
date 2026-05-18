3676b137e7dc7dc8b6bf3e24403f5d7fcc288ee2
Simon Cun <simoncun2586@gmail.com>
2026-05-18 16:06:38 -0700

    File updated at 5/18/2026, 4:06:38 PM

--- 1306-JumpGameIII/1306-JumpGameIII.cpp
+++ 1306-JumpGameIII/1306-JumpGameIII.cpp
@@ -1,14 +1,22 @@
-// Last updated: 5/17/2026, 9:00:49 AM
+// Last updated: 5/18/2026, 4:06:38 PM
 1class Solution {
-2public:
-3    bool canReach(vector<int>& arr, int start) {
-4        unordered_set<int> visited;
-5        return canReachHelp(arr, start, visited);
-6    }
-7    bool canReachHelp(vector<int>& arr, int start, unordered_set<int>& visited) {
-8        if (start < 0 or start > (arr.size() - 1) or visited.contains(start)) return false;
-9        if (arr[start] == 0) return true;
-10        visited.insert(start);
-11        return canReachHelp(arr, start - arr.at(start), visited) + canReachHelp(arr, start + arr.at(start), visited);
-12    }
-13};
+2    bool helper(vector<int>& arr, int idx, vector<bool>& visited) {
+3        if (idx < 0 || idx >= arr.size()) return false;
+4        if (visited[idx]) return false;
+5        if (arr[idx] == 0) return true;
+6
+7        visited[idx] = true;
+8
+9        bool right = helper(arr, idx + arr[idx], visited);
+10        bool left = helper(arr, idx - arr[idx], visited);
+11
+12        if (right || left) return true;
+13
+14        return false;
+15    }
+16public:
+17    bool canReach(vector<int>& arr, int start) {
+18        vector<bool> visited(arr.size(), false);
+19        return helper(arr, start, visited);
+20    }
+21};
