01ad400102479efa0fd62aa1baacd6a00f76a93f
Simon Cun <simoncun2586@gmail.com>
2026-05-20 10:13:47 -0700

    File updated at 5/20/2026, 10:13:47 AM

--- 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.cpp
+++ 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/20/2026, 10:10:52 AM
+// Last updated: 5/20/2026, 10:13:47 AM
 1class Solution {
 2public:
 3    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
@@ -6,15 +6,13 @@
 5        vector<int> ret;
 6        unordered_set<int> s;
 7        int common = 0;
-8        int i = 0;
-9        while (i < n) {
-10            if (s.contains(A[i])) ++common;
-11            else s.insert(A[i]);
-12            if (s.contains(B[i])) ++common;
-13            else s.insert(B[i]);
-14            ret.push_back(common);
-15            ++i;
-16        }
-17        return ret;
-18    }
-19};
+8        for (int i = 0; i < n; ++i) {
+9            if (s.contains(A[i])) ++common;
+10            else s.insert(A[i]);
+11            if (s.contains(B[i])) ++common;
+12            else s.insert(B[i]);
+13            ret.push_back(common);
+14        }
+15        return ret;
+16    }
+17};
