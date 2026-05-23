7afa3c1dadb77c69f59dcd7ad2a3947fc0970447
Simon Cun <simoncun2586@gmail.com>
2026-05-23 10:23:53 -0700

    Create 1752-CheckifArrayIsSortedandRotated

---
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/23/2026, 10:23:53 AM
+1class Solution {
+2public:
+3    bool check(vector<int>& nums) {
+4        int count = 0;
+5        for (int i = 1; i < nums.size() * 2; ++i) {
+6            if (nums.at((i - 1) % nums.size()) > nums.at(i % nums.size())) {
+7                ++count;
+8            }
+9            if (count == 3) return false;
+10        }
+11        return true;
+12    }
+13};
