45caa03b3b4b1b9c6e0cacc8df3e010767414a41
Simon Cun <simoncun2586@gmail.com>
2026-07-24 14:05:07 -0700

    File updated at 7/24/2026, 2:05:07 PM

--- 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
+++ 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
@@ -1,16 +1,18 @@
-// Last updated: 7/24/2026, 10:07:56 AM
+// Last updated: 7/24/2026, 2:05:07 PM
 1#include <algorithm>
 2#include <vector>
 3class Solution {
 4public:
-5    int findGCD(vector<int>& nums) {
-6        int small = *min_element(nums.begin(), nums.end()), large = *max_element(nums.begin(), nums.end());
-7        int gcd = 1;
-8        for (int i = 1; i <= large; ++i) {
-9            if (small % i == 0 && large % i == 0) {
-10                gcd = i;
-11            }
-12        }
-13        return gcd;
-14    }
-15};
+5    int gcd(int a, int b) {
+6        while (a % b != 0) {
+7            int r = a % b;
+8            a = b;
+9            b = r;
+10        }
+11        return b;
+12    }
+13    int findGCD(vector<int>& nums) {
+14        int small = *min_element(nums.begin(), nums.end()), large = *max_element(nums.begin(), nums.end());
+15        return gcd(large, small);
+16    }
+17};
