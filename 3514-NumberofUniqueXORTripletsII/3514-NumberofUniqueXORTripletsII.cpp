ba70bdf2d3ded03704c66e2d57ec84b803312336
Simon Cun <simoncun2586@gmail.com>
2026-07-24 09:58:25 -0700

    File updated at 7/24/2026, 9:58:24 AM

--- 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
+++ 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
@@ -1,19 +1,21 @@
-// Last updated: 7/24/2026, 9:09:24 AM
+// Last updated: 7/24/2026, 9:58:24 AM
 1class Solution {
 2public:
-3    int uniqueXorTriplets(vector<int>& nums) {
-4        unordered_set<int> arr;
-5        for (int i = 0; i < nums.size(); ++i) {
-6            for (int j = 0; j < nums.size(); ++j) {
-7                arr.insert(nums.at(i) ^ nums.at(j));
-8            }
-9        }
-10        unordered_set<int> hash;
-11        for (auto& i : nums) {
-12            for (auto& j : arr) {
-13                hash.insert(i ^ j);
+3    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+4        int n = grid.size(), m = grid.at(0).size();
+5        vector<vector<int>> arr = grid;
+6        for (int u = 0; u < k; ++u) {
+7            for (int i = 0; i < n; ++i) {
+8                for (int j = 0; j < m - 1; ++j) {
+9                    arr[i][j + 1] = grid.at(i).at(j);
+10                }
+11            }
+12            for (int i = 0; i < n - 1; ++i) {
+13                arr[i + 1][0] = grid.at(i).at(m - 1);
 14            }
-15        }
-16        return hash.size();
-17    }
-18};
+15            arr.at(0).at(0) = grid[n - 1][m - 1];
+16            grid = arr;
+17        }
+18        return arr;
+19    }
+20};
