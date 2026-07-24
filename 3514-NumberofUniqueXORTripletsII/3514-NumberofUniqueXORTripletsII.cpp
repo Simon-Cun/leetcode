880368add902bda61dc2903cd4cb6034d0bc23df
Simon Cun <simoncun2586@gmail.com>
2026-07-24 14:10:41 -0700

    File updated at 7/24/2026, 2:10:40 PM

--- 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
+++ 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
@@ -1,11 +1,11 @@
-// Last updated: 7/24/2026, 2:09:42 PM
+// Last updated: 7/24/2026, 2:10:40 PM
 1class Solution {
 2public:
 3    int uniqueXorTriplets(vector<int>& nums) {
 4        int n = nums.size();
 5        if (n < 3) return n;
 6        int ret = 1;
-7        while (n >= pow(2, ret)) ++ret;
-8        return 1 << ret;
+7        while (n >= ret) ret <<= 1;
+8        return ret;
 9    }
 10};
