fc740c0afb22b6ba5a0caada321c972f1eec475b
Simon Cun <simoncun2586@gmail.com>
2026-07-24 10:00:49 -0700

    File updated at 7/24/2026, 10:00:49 AM

--- 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
+++ 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
@@ -1,19 +1,19 @@
-// Last updated: 7/24/2026, 9:59:01 AM
+// Last updated: 7/24/2026, 10:00:49 AM
 1class Solution {
 2public:
 3    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-4        int n = grid.size(), m = grid.at(0).size();
+4        int m = grid.size(), n = grid.at(0).size();
 5        vector<vector<int>> arr = grid;
-6        for (int u = 0; u < k; ++u) {
-7            for (int i = 0; i < n; ++i) {
-8                for (int j = 0; j < m - 1; ++j) {
+6        while (k--) {
+7            for (int i = 0; i < m; ++i) {
+8                for (int j = 0; j < n - 1; ++j) {
 9                    arr[i][j + 1] = grid.at(i).at(j);
 10                }
 11            }
-12            for (int i = 0; i < n - 1; ++i) {
-13                arr[i + 1][0] = grid.at(i).at(m - 1);
+12            for (int i = 0; i < m - 1; ++i) {
+13                arr[i + 1][0] = grid.at(i).at(n - 1);
 14            }
-15            arr.at(0).at(0) = grid[n - 1][m - 1];
+15            arr.at(0).at(0) = grid[m - 1][n - 1];
 16            grid = arr;
 17        }
 18        return arr;
