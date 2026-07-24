6653cbe53cae7fc9fc2315d7edce69ed978f76dc
Simon Cun <simoncun2586@gmail.com>
2026-07-24 15:03:15 -0700

    File updated at 7/24/2026, 3:03:15 PM

--- 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
+++ 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
@@ -1,21 +1,23 @@
-// Last updated: 7/24/2026, 2:32:31 PM
+// Last updated: 7/24/2026, 3:03:15 PM
 1class Solution {
 2public:
-3    vector<int> arrayRankTransform(vector<int>& arr) {
-4        unordered_set<int> tmp;
-5        for (auto& i : arr) tmp.insert(i);
-6        vector<int> rankings;
-7        for (auto& i : tmp) rankings.push_back(i);
-8        sort(rankings.begin(), rankings.end());
-9        unordered_map<int, int> hash;
-10        for (int i = 0; i < rankings.size(); ++i) {
-11            hash[rankings.at(i)] = i + 1;
+3    long long sumAndMultiply(long long n) {
+4        if (n == 0) return 0;
+5        vector<long long> nums;
+6        while (n != 0) {
+7            if (n % 10 != 0) {
+8            nums.push_back(n % 10);
+9
+10            }
+11            n /= 10;
 12        }
-13        for (auto& i : hash) cout << i.first << ' ' << i.second << endl;
-14        vector<int> res;
-15        for (auto& i : arr) {
-16            res.push_back(hash[i]);
-17        }
-18        return res;
-19    }
-20};
+13        reverse(nums.begin(), nums.end());
+14        int multiplier = accumulate(nums.begin(), nums.end(), 0);
+15        vector<string> arr;
+16        for (auto& i : nums) arr.push_back(to_string(i));
+17        string num = "";
+18        for (auto& i : arr) num += i;
+19        long long ret = stoi(num);
+20        return ret * multiplier;
+21    }
+22};
