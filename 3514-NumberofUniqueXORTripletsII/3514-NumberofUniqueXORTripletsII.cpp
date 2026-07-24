e75ac631adf52a3dcfdc1b2ed9338a674dbfb912
Simon Cun <simoncun2586@gmail.com>
2026-07-24 10:07:57 -0700

    File updated at 7/24/2026, 10:07:56 AM

--- 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
+++ 3514-NumberofUniqueXORTripletsII/3514-NumberofUniqueXORTripletsII.cpp
@@ -1,21 +1,16 @@
-// Last updated: 7/24/2026, 10:02:19 AM
-1class Solution {
-2public:
-3    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-4        int m = grid.size(), n = grid.at(0).size();
-5        vector<vector<int>> ret = grid;
-6        while (k--) {
-7            for (int i = 0; i < m; ++i) {
-8                for (int j = 0; j < n - 1; ++j) {
-9                    ret.at(i).at(j + 1) = grid.at(i).at(j);
-10                }
+// Last updated: 7/24/2026, 10:07:56 AM
+1#include <algorithm>
+2#include <vector>
+3class Solution {
+4public:
+5    int findGCD(vector<int>& nums) {
+6        int small = *min_element(nums.begin(), nums.end()), large = *max_element(nums.begin(), nums.end());
+7        int gcd = 1;
+8        for (int i = 1; i <= large; ++i) {
+9            if (small % i == 0 && large % i == 0) {
+10                gcd = i;
 11            }
-12            for (int i = 0; i < m - 1; ++i) {
-13                ret.at(i + 1).at(0) = grid.at(i).at(n - 1);
-14            }
-15            ret.at(0).at(0) = grid.at(m - 1).at(n - 1);
-16            grid = ret;
-17        }
-18        return ret;
-19    }
-20};
+12        }
+13        return gcd;
+14    }
+15};
