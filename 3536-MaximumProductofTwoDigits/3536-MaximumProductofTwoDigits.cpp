0737832496770a44e6596c0c7855b05cf6afaac1
Simon Cun <simoncun2586@gmail.com>
2026-07-25 20:28:06 -0700

    File updated at 7/25/2026, 8:28:06 PM

--- 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.cpp
+++ 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.cpp
@@ -1,23 +1,18 @@
-// Last updated: 7/25/2026, 8:24:41 PM
+// Last updated: 7/25/2026, 8:28:06 PM
 1class Solution {
 2public:
 3    int maxProduct(int n) {
-4        vector<vector<int>> buckets(10);
+4        int curr = 0, prev = 0;
 5        while (n) {
 6            int rem = n % 10;
 7            n /= 10;
-8            buckets.at(rem).push_back(rem);
-9        }
-10        reverse(buckets.begin(), buckets.end());
-11        vector<int> nums;
-12        for (auto& i : buckets) {
-13            for (auto& j : i) {
-14                nums.push_back(j);
-15                if (nums.size() == 2) {
-16                    return nums.at(0) * nums.at(1);
-17                }
-18            }
-19        }
-20        return -1;
-21    }
-22};
+8            if (curr < rem) {
+9                prev = curr;
+10                curr = rem;
+11            } else if (prev < rem){
+12                prev = rem;
+13            }
+14        }
+15        return prev * curr;
+16    }
+17};
