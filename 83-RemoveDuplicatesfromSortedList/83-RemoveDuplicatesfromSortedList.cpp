e035d7f14dd06dbf268f4ef6e761a68c9457d948
Simon Cun <simoncun2586@gmail.com>
2026-07-26 10:07:17 -0700

    File updated at 7/26/2026, 10:07:16 AM

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/24/2026, 4:56:13 PM
+// Last updated: 7/26/2026, 10:07:16 AM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -12,18 +12,17 @@
 11class Solution {
 12public:
 13    ListNode* deleteDuplicates(ListNode* head) {
-14        ListNode* prev = nullptr;
+14        if (!head) return head;
 15        ListNode* curr = head;
-16        while (curr) {
-17            if (prev and prev->val == curr->val) {
-18                prev->next = curr->next;
-19                delete curr;
-20                curr = prev->next;
+16        while (curr->next) {
+17            if (curr->val == curr->next->val) {
+18                ListNode* tmp = curr->next;
+19                curr->next = curr->next->next;
+20                delete tmp;
 21            } else {
-22                prev = curr;
-23                curr = curr->next;
-24            }
-25        }
-26        return head;
-27    }
-28};
+22                curr = curr->next;
+23            }
+24        }
+25        return head;
+26    }
+27};
