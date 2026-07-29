c2228e40b18b95ccf799d5f0bdf57e076ff27818
Simon Cun <simoncun2586@gmail.com>
2026-07-28 22:54:58 -0700

    File updated at 7/28/2026, 10:54:58 PM

--- 77-Combinations/77-Combinations.cpp
+++ 77-Combinations/77-Combinations.cpp
@@ -1,23 +1,21 @@
-// Last updated: 7/28/2026, 10:50:46 PM
+// Last updated: 7/28/2026, 10:54:58 PM
 1class Solution {
 2public:
-3    void backtrack(int n, int k, unordered_set<int> v, vector<vector<int>>& res, vector<int> curr) {
+3    void backtrack(int n, int k, int start, vector<vector<int>>& res, vector<int>& curr) {
 4        if (k == curr.size()) {
 5            res.push_back(curr);
 6            return;
 7        }
-8        for (int  i = 1; i <= n; ++i) {
-9            if (!v.contains(i)) {
-10                v.insert(i);
-11                curr.push_back(i);
-12                backtrack(n, k, v, res, curr);
-13                curr.pop_back();
-14            }
-15        }
-16    }
-17    vector<vector<int>> combine(int n, int k) {
-18        vector<vector<int>> res;
-19        backtrack(n, k, unordered_set<int>(), res, vector<int>());
-20        return res;
-21    }
-22};
+8        for (int  i = start; i <= n; ++i) {
+9            curr.push_back(i);
+10            backtrack(n, k, i + 1, res, curr);
+11            curr.pop_back();
+12        }
+13    }
+14    vector<vector<int>> combine(int n, int k) {
+15        vector<vector<int>> res;
+16        vector<int> curr;
+17        backtrack(n, k, 1, res, curr);
+18        return res;
+19    }
+20};
