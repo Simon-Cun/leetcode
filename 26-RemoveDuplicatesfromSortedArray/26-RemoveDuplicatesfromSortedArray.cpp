7bcbb09ab31322e3d4368335c4fafa261ecc6c77
Simon Cun <simoncun2586@gmail.com>
2026-07-28 23:27:43 -0700

    File updated at 7/28/2026, 11:27:43 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -1,9 +1,9 @@
-// Last updated: 7/26/2026, 10:10:42 AM
+// Last updated: 7/28/2026, 11:27:43 PM
 1class Solution {
 2public:
 3    int removeDuplicates(vector<int>& nums) {
 4        int k = 1;
-5        for (int i = 1; i < nums.size(); ++i) {
+5        for (int i = k; i < nums.size(); ++i) {
 6            if (nums.at(i) != nums.at(i - 1)) {
 7                nums.at(k++) = nums.at(i);
 8            }
