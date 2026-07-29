5221e001596f0a5ee03cf37720ca6ab511e1aa4c
Simon Cun <simoncun2586@gmail.com>
2026-07-28 23:18:35 -0700

    File updated at 7/28/2026, 11:18:34 PM

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,15 +1,15 @@
-// Last updated: 7/28/2026, 11:15:37 PM
+// Last updated: 7/28/2026, 11:18:34 PM
 1class Solution {
 2public:
 3    void sortColors(vector<int>& nums) {
-4        int l = 0, r = nums.size() - 1, m = 0;
+4        int l = 0, m = 0, r = nums.size() - 1;
 5        while (m <= r) {
 6            if (nums.at(m) == 0) {
-7                swap(nums.at(l), nums.at(m));
+7                swap(nums.at(m), nums.at(l));
 8                ++l;
 9                ++m;
 10            } else if (nums.at(m) == 2) {
-11                swap(nums.at(r), nums.at(m));
+11                swap(nums.at(m), nums.at(r));
 12                --r;
 13            } else {
 14                ++m;
