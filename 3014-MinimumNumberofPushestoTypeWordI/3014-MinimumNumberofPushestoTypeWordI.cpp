1a65d5059b2a930abaef60dfc4fdeca7807df79d
Simon Cun <simoncun2586@gmail.com>
2026-07-29 19:21:26 -0700

    File updated at 7/29/2026, 7:21:26 PM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
@@ -1,18 +1,8 @@
-// Last updated: 7/29/2026, 7:16:54 PM
+// Last updated: 7/29/2026, 7:21:26 PM
 1class Solution {
 2public:
-3    string smallestPalindrome(string s) {
-4        if (s.size() == 1) return s;
-5        string m = "";
-6        if (s.size() % 2) m = s.at(s.size() / 2);
-7        vector<int> charMap(26, 0);
-8        for (int i = 0; i < s.size() / 2; ++i) ++charMap.at(s.at(i) - 'a');
-9        string half1 = "";
-10        for (int i = 0; i < charMap.size(); ++i) {
-11            if (charMap.at(i) != 0) for (int j = 0; j < charMap.at(i); ++j) half1 += (i + 'a');
-12        }
-13        string half2 = half1;
-14        reverse(half2.begin(), half2.end());
-15        return (half1 + m + half2);
-16    }
-17};
+3    int maxProduct(vector<int>& nums) {
+4        sort(nums.begin(), nums.end(), greater());
+5        return (nums.at(0) - 1) * (nums.at(1) - 1);
+6    }
+7};
