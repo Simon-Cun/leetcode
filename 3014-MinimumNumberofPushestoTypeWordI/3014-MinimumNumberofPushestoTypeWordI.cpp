36be7630eebe4b5a04cc1cda2a59c894ad457e63
Simon Cun <simoncun2586@gmail.com>
2026-07-29 19:26:33 -0700

    File updated at 7/29/2026, 7:26:33 PM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
@@ -1,8 +1,16 @@
-// Last updated: 7/29/2026, 7:21:26 PM
+// Last updated: 7/29/2026, 7:26:33 PM
 1class Solution {
 2public:
 3    int maxProduct(vector<int>& nums) {
-4        sort(nums.begin(), nums.end(), greater());
-5        return (nums.at(0) - 1) * (nums.at(1) - 1);
-6    }
-7};
+4        int prev = 0, curr = 0;
+5        for (int i = 0; i < nums.size(); ++i) {
+6            if (nums.at(i) > curr) {
+7                prev = curr;
+8                curr = nums.at(i);
+9            } else if (prev < nums.at(i)) {
+10                prev = nums.at(i);
+11            }
+12        }
+13        return (prev - 1) * (curr - 1);
+14    }
+15};
