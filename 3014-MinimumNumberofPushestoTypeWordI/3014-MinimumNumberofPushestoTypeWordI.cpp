74880569e737ed17371f066bb75565976a02333a
Simon Cun <simoncun2586@gmail.com>
2026-07-29 19:16:48 -0700

    File updated at 7/29/2026, 7:16:48 PM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.cpp
@@ -1,18 +1,18 @@
-// Last updated: 7/29/2026, 6:54:46 PM
+// Last updated: 7/29/2026, 7:16:48 PM
 1class Solution {
 2public:
-3    int minimumPushes(string word) {
-4        vector<int> charMap(26, 0);
-5        for (auto& i : word) ++charMap.at(i - 'a');
-6        sort(charMap.begin(), charMap.end(), greater());
-7        int summation = 1;
-8        int res = 0;
-9        int count = 0;
-10        for (auto& i : charMap) {
-11            if (count > 0 && count % 8 == 0) ++summation;
-12            res += summation * i;
-13            ++count;
-14        }
-15        return res;
+3    string smallestPalindrome(string s) {
+4        if (s.size() == 1) return s;
+5        string m = "";
+6        if (s.size() % 2) m = s.at(s.size() / 2);
+7        vector<int> charMap(26, 0);
+8        for (int i = 0; i < s.size() / 2; ++i) ++charMap.at(s.at(i) - 'a');
+9        string half1 = "";
+10        for (int i = 0; i < charMap.size(); ++i) {
+11            if (charMap.at(i) != 0) for (int j = 0; j < charMap.at(i); ++j) half1 += (i + 'a');
+12        }
+13        string half2 = half1;
+14        reverse(half2.begin(), half2.end());
+15        return (half1 + m + half2);
 16    }
 17};
