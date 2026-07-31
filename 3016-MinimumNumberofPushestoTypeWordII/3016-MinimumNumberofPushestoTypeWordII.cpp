81bef8335d47aefed72226b493fd4036b04b66a9
Simon Cun <simoncun2586@gmail.com>
2026-07-30 20:33:24 -0700

    Create 3016-MinimumNumberofPushestoTypeWordII

---
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/30/2026, 8:33:23 PM
+1class Solution {
+2public:
+3    int minimumPushes(string word) {
+4        vector<int> charMap(26, 0);
+5        for (auto& i : word) ++charMap.at(i - 'a');
+6        sort(charMap.begin(), charMap.end(), greater());
+7        int summation = 1;
+8        int res = 0;
+9        int count = 0;
+10        for (auto& i : charMap) {
+11            if (count > 0 && count % 8 == 0) ++summation;
+12            res += summation * i;
+13            ++count;
+14        }
+15        return res;
+16    }
+17};
