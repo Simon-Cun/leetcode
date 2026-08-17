ca02f88b1a9896cbd80abd34daeecd99e2f29f8b
Simon Cun <simoncun2586@gmail.com>
2026-08-16 21:17:33 -0700

    File updated at 8/16/2026, 9:17:32 PM

--- 45-JumpGameII/45-JumpGameII.cpp
+++ 45-JumpGameII/45-JumpGameII.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/16/2026, 9:16:48 PM
+// Last updated: 8/16/2026, 9:17:32 PM
 1class Solution {
 2public:
 3    int dp(vector<int>& nums, int index, unordered_map<int, int>& visited) {
@@ -8,13 +8,16 @@
 7        for (int i = index + 1; i <= min((int)nums.size(), index + nums.at(index)); ++i) {
 8            res = min(res, dp(nums, i, visited));
 9        }
-10        if (res == INT_MAX) return INT_MAX;
-11        visited[index] = res + 1;
-12        return res + 1;
-13    }
-14    int jump(vector<int>& nums) {
-15        unordered_map<int, int> visited;
-16        int ret = dp(nums, 0, visited);
-17        return ret;
-18    }
-19};
+10        if (res == INT_MAX) {
+11            visited[index] = INT_MAX;
+12            return INT_MAX;
+13        }
+14        visited[index] = res + 1;
+15        return res + 1;
+16    }
+17    int jump(vector<int>& nums) {
+18        unordered_map<int, int> visited;
+19        int ret = dp(nums, 0, visited);
+20        return ret;
+21    }
+22};
