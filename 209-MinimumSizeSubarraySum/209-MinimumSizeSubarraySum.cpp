72b0ff3f27658183904db9ba82feeb940cde7be4
Simon Cun <simoncun2586@gmail.com>
2026-08-22 10:43:13 -0700

    File updated at 8/22/2026, 10:43:13 AM

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.cpp
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.cpp
@@ -1,21 +1,17 @@
-// Last updated: 8/21/2026, 11:51:25 PM
+// Last updated: 8/22/2026, 10:43:13 AM
 1class Solution {
 2public:
 3    int minSubArrayLen(int target, vector<int>& nums) {
-4        int curr = 0;
-5        if (accumulate(nums.begin(), nums.end(),0) < target) return 0;
-6        int ret= INT_MAX;
-7        int l = 0, r = 0;
-8        while (r < nums.size()) {
-9            curr += nums.at(r);
-10            ++r;
-11            while (curr >= target) {
-12                ret = min(ret, r - l);
-13                curr -= nums.at(l);
-14                ++l;
-15            }
-16            
-17        }
-18        return ret;
-19    }
-20};
+4        int l = 0;
+5        int curr = 0, ret = INT_MAX;
+6        for (int r = 0; r < nums.size(); ++r) {
+7            curr += nums.at(r);
+8            while (curr >= target) {
+9                ret = min(ret, r - l + 1);
+10                curr -= nums.at(l);
+11                ++l;
+12            }
+13        }
+14        return (ret == INT_MAX) ? 0 : ret;
+15    }
+16};
