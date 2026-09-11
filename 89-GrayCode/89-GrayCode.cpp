5c3debfa57b3c92e3e0be254720240732eacfe83
Simon Cun <simoncun2586@gmail.com>
2026-09-11 16:10:18 -0700

    File updated at 9/11/2026, 4:10:18 PM

--- 89-GrayCode/89-GrayCode.cpp
+++ 89-GrayCode/89-GrayCode.cpp
@@ -1,36 +1,30 @@
-// Last updated: 9/8/2026, 12:38:49 PM
+// Last updated: 9/11/2026, 4:10:18 PM
 1class Solution {
 2public:
-3    vector<int> backtrack(int n, int m, unordered_set<int>& visited, vector<int>& ret) {
-4        ret.push_back(m);
-5        if (ret.size() == pow(2, n)) {
-6            return ret;
-7        }
-8        
-9        vector<int> res;
-10        int bitmask = 1;
-11        while (bitmask < (1 << n)) {
-12            if (visited.contains(m ^ bitmask)) {
-13                bitmask *= 2;
-14                continue;
-15            }
-16            m ^= bitmask;
-17            visited.insert(m);
-18            res = backtrack(n, m, visited, ret);
-19            visited.erase(m);
-20            m ^= bitmask;
-21            if (res.size() == pow(2, n)) {
-22                return res;
-23            }
-24            bitmask *= 2;
-25        }
-26        ret.pop_back();
-27        return res;
+3    vector<int> backtrack(int n, int m, vector<int>& curr, unordered_set<int>& v) {
+4        curr.push_back(m);
+5        if (curr.size() == pow(2, n)) return curr;
+6        vector<int> ret;
+7        int bitmask = 1;
+8        while (bitmask < 1 << n) {
+9            if (v.contains(m ^ bitmask)) {
+10                bitmask *= 2;
+11                continue;
+12            }
+13            m ^= bitmask;
+14            v.insert(m);
+15            ret = backtrack(n, m, curr, v);
+16            v.erase(m);
+17            m ^= bitmask;
+18            if (curr.size() == pow(2, n)) return ret;
+19        }
+20        curr.pop_back();
+21        return ret;
+22    }
+23    vector<int> grayCode(int n) {
+24        vector<int> ret;
+25        unordered_set<int> v;
+26        v.insert(0);
+27        return backtrack(n, 0, ret, v);
 28    }
-29    vector<int> grayCode(int n) {
-30        vector<int> ret;
-31        unordered_set<int> v;
-32        v.insert(0);
-33        return backtrack(n, 0, v, ret);
-34    }
-35};
+29};
