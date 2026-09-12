c1d362094d32c3e651efa2cd3228424d8217823d
Simon Cun <simoncun2586@gmail.com>
2026-09-11 17:41:43 -0700

    File updated at 9/11/2026, 5:41:43 PM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.cpp
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 9/11/2026, 5:18:52 PM
+// Last updated: 9/11/2026, 5:41:43 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -12,26 +12,21 @@
 11 */
 12class Solution {
 13public:
-14    void inorder(TreeNode* curr, vector<TreeNode*>& arr) {
+14    void inorder(TreeNode* curr, TreeNode*& prev, TreeNode*& first, TreeNode*& second) {
 15        if (!curr) return;
-16        inorder(curr->left, arr);
-17        arr.push_back(curr);
-18        inorder(curr->right, arr);
-19    }
-20    static bool compare(TreeNode* a, TreeNode* b) {
-21        return a->val < b->val;
-22    }
-23    void recoverTree(TreeNode* root) {
-24        vector<TreeNode*> arr;
-25        inorder(root, arr);
-26        vector<TreeNode*> tmp = arr;
-27        sort(tmp.begin(), tmp.end(), compare);
-28        TreeNode* first = nullptr;
-29        TreeNode* second = nullptr;
-30        for (int i = 0; i < tmp.size(); ++i) {
-31            if (!first && tmp.at(i) != arr.at(i)) first = arr.at(i);
-32            else if (!second && tmp.at(i) != arr.at(i)) second = arr.at(i);
-33        }
-34        swap(first->val, second->val);
-35    }
-36};
+16        inorder(curr->left, prev, first, second);
+17        if (prev && prev->val >= curr->val) {
+18            if (!first) first = prev;
+19            second = curr;
+20        }
+21        prev = curr;
+22        inorder(curr->right, prev, first, second);
+23    }
+24    void recoverTree(TreeNode* root) {
+25        TreeNode* first = nullptr;
+26        TreeNode* second = nullptr;
+27        TreeNode* prev = nullptr;
+28        inorder(root, prev, first, second);
+29        swap(first->val, second->val);
+30    }
+31};
